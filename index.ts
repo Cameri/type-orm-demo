f665eed5e12fc03ceb3dd1d6a83c71e4f7c39eec
Vlad R <vromanov@nestwealth.com>
2018-11-21 13:57:29 -0500

    added mongod db

--- index.ts
+++ index.ts
@@ -1,34 +1,71 @@
-import { createConnection, Connection, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { createConnection, Connection, Entity, PrimaryGeneratedColumn, Column, ObjectIdColumn } from 'typeorm';
+import { ObjectID } from 'mongodb';
 
 @Entity()
 class WebsiteUser {
     @PrimaryGeneratedColumn()
-    public id: string;
+    public id?: number;
 
     @Column()
     public email: string;
 }
 
-async function main() {
-    const connection: Connection = await createConnection({
-        type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "nestwealth",
-        database: "nestwealth",
-        entities: [WebsiteUser]
-    });
+@Entity({
+    name: 'tombstones'
+})
+class Tombstone {
+    @ObjectIdColumn()
+    public id?: ObjectID;
 
-    const UserRepository = connection.getRepository(WebsiteUser);
+    @Column()
+    public email: string;
 
-    const user = await UserRepository.findOne(1);
-
-    console.log(user);
+    @Column()
+    public AHEmail: string;
 
 }
 
-main().then(() => {
-    console.log('done');
-    process.exit(0);
-})
+async function main() {
+    const mysqlConnection: Connection = await createConnection({
+        name: 'mysql',
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'nestwealth',
+        database: 'nestwealth',
+        entities: [WebsiteUser]
+    });
+
+    const mongoConnection: Connection = await createConnection({
+        name: 'mongodb',
+        type: 'mongodb',
+        host: 'localhost',
+        port: 27017,
+        database: 'nestwealth',
+        useNewUrlParser: true,
+        entities: [Tombstone]
+    });
+
+    const UserRepository = mysqlConnection.getRepository(WebsiteUser);
+    const user = await UserRepository.findOne({
+        id: 1
+    });
+    if (!user) return;
+
+    const TombstoneRepository = mongoConnection.getMongoRepository(Tombstone);
+
+    const [tombstone] = await TombstoneRepository.findByIds([new ObjectID('5841d8f3f3f0e7581c7bca54')]);
+
+    console.log(user);
+    console.log(tombstone);
+}
+
+main()
+    .then(() => {
+        console.log('done');
+        process.exit(0);
+    }).catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
