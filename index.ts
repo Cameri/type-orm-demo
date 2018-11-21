8d2e856dede7bc7ac17040651a5655f8c6230e2d
Vlad R <vromanov@nestwealth.com>
2018-11-21 14:05:00 -0500

    added prettier

--- index.ts
+++ index.ts
@@ -1,71 +1,86 @@
-import { createConnection, Connection, Entity, PrimaryGeneratedColumn, Column, ObjectIdColumn } from 'typeorm';
+import {
+  createConnection,
+  Connection,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ObjectIdColumn,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
 import { ObjectID } from 'mongodb';
 
 @Entity()
 class WebsiteUser {
-    @PrimaryGeneratedColumn()
-    public id?: number;
+  @PrimaryGeneratedColumn()
+  public id?: number;
 
-    @Column()
-    public email: string;
+  @Column()
+  public email: string;
+
+  @OneToMany(type => Tombstone, tombstone => tombstone.user)
+  public tombstone: Tombstone;
 }
 
 @Entity({
-    name: 'tombstones'
+  name: 'tombstones',
 })
 class Tombstone {
-    @ObjectIdColumn()
-    public id?: ObjectID;
+  @ObjectIdColumn()
+  public id?: ObjectID;
 
-    @Column()
-    public email: string;
+  @Column()
+  public email: string;
 
-    @Column()
-    public AHEmail: string;
+  @Column()
+  public AHEmail: string;
 
+  @ManyToOne(type => WebsiteUser, user => user.tombstone)
+  public user: WebsiteUser;
 }
 
 async function main() {
-    const mysqlConnection: Connection = await createConnection({
-        name: 'mysql',
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'nestwealth',
-        database: 'nestwealth',
-        entities: [WebsiteUser]
-    });
+  const mysqlConnection: Connection = await createConnection({
+    name: 'mysql',
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'nestwealth',
+    database: 'nestwealth',
+    entities: [WebsiteUser],
+  });
 
-    const mongoConnection: Connection = await createConnection({
-        name: 'mongodb',
-        type: 'mongodb',
-        host: 'localhost',
-        port: 27017,
-        database: 'nestwealth',
-        useNewUrlParser: true,
-        entities: [Tombstone]
-    });
+  const mongoConnection: Connection = await createConnection({
+    name: 'mongodb',
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    database: 'nestwealth',
+    useNewUrlParser: true,
+    entities: [Tombstone],
+  });
 
-    const UserRepository = mysqlConnection.getRepository(WebsiteUser);
-    const user = await UserRepository.findOne({
-        id: 1
-    });
-    if (!user) return;
+  const UserRepository = mysqlConnection.getRepository(WebsiteUser);
+  const user = await UserRepository.findOne({
+    id: 1,
+  });
+  if (!user) return;
 
-    const TombstoneRepository = mongoConnection.getMongoRepository(Tombstone);
+  // const TombstoneRepository = mongoConnection.getMongoRepository(Tombstone);
 
-    const [tombstone] = await TombstoneRepository.findByIds([new ObjectID('5841d8f3f3f0e7581c7bca54')]);
+  // const [tombstone] = await TombstoneRepository.findByIds([new ObjectID('5841d8f3f3f0e7581c7bca54')]);
 
-    console.log(user);
-    console.log(tombstone);
+  console.log(user);
+  // console.log(tombstone);
 }
 
 main()
-    .then(() => {
-        console.log('done');
-        process.exit(0);
-    }).catch(e => {
-        console.error(e);
-        process.exit(1);
-    })
+  .then(() => {
+    console.log('done');
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
